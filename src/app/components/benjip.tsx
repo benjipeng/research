eac4a3dcad3ad0685203b6d3639b0dc9ef76d901
B. P. Hon <baijiephoneycutt@gmail.com>
2023-07-17 12:04:56 -0400

    update header and h1/h2

---
+++ src/app/components/benjip.tsx
@@ -0,0 +1,128 @@
+function BenjiH1() {
+  return (
+    <>
+      <div className="mt-8">
+        <h1 className="text-center">
+          <span className="font-semibold text-4xl text-indigo-600/90">
+            Benji Peng
+          </span>
+          <span className="text-4xl">&nbsp;</span>
+          <span className="font-medium italic text-2xl">
+            Ph.D. in Chemisrty
+          </span>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+const BenjiHeadline = () => {
+  return (
+    <>
+      <h2 className="max-w-screen-lg m-auto text-left font-medium text-lg mt-4">
+        Fueling Innovation & Scientific Discoveries and &nbsp;
+        <span className="underline decoration-double decoration-cyan-500/30 hover:decoration-emerald-500/30">
+          Empowering Researchers
+        </span>
+        &nbsp; in Life Sciences via {"  "}
+        <span className="underline decoration-wavy decoration-violet-500/50 hover:decoration-rose-500/60">
+          the Design of Advanced Analytical Methodologies
+        </span>
+        , {"  "}
+        <span className="underline decoration-wavy decoration-blue-500/50 hover:decoration-rose-500/60">
+          Fostering a Deeper Understanding of Complex Chemical Phenomena
+        </span>
+        , and {""}
+        <span className="underline decoration-wavy decoration-green-500/50 hover:decoration-rose-500/60">
+          the Integration of Cutting-Edge Computational Tools and
+          Instrumentation
+        </span>
+      </h2>
+    </>
+  );
+};
+
+const BenjiTags = () => {
+  return (
+    <>
+      <div className="flex my-4 flex-row place-content-center">
+        <div className="badge badge badge-neutral">
+          <span className="font-semibold">Analytical Chemistry</span>
+        </div>
+        <div className="badge badge badge-primary">
+          <span className="font-semibold">Physical Chemistry</span>
+        </div>
+        <div className="badge badge badge-ghost">
+          <span className="font-semibold">Biophysics</span>
+        </div>
+      </div>
+      <div className="my-2 mx-auto max-w-screen-xl">
+        <div className="flex flex-row flex-auto flex-wrap gap-2 place-content-center m-auto">
+          <div className="badge badge-sm badge-outline badge-ghost">
+            Gas / Liquid Chromtography
+          </div>
+          <div className="badge badge-sm badge-outline badge-ghost">
+            Mass spectrometry
+          </div>
+          <div className="badge badge-sm badge-outline badge-ghost">
+            Chromatography simulation
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Laser Optics
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Cellular Imaging
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Confocal microscopy
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Digital Signal Processing (DSP)
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Optical Modulation
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Super-resolution imaging
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Scanning Tunneling Microscopy (STM)
+          </div>
+          <div className="badge badge-sm badge-outline badge-primary">
+            Transmission Electron Microscopy (TEM)
+          </div>
+          <div className="badge badge-sm badge-outline badge-info">
+            Fluorescent Protein (BFP, YFP, GFP, RFP)
+          </div>
+          <div className="badge badge-sm badge-outline badge-info">
+            X-ray crystallography
+          </div>
+          <div className="badge badge-sm badge-outline badge-info">
+            Site-directed mutagenesis (SDM)
+          </div>
+          <div className="badge badge-sm badge-outline badge-info">
+            X-ray crystallography
+          </div>
+          <div className="badge badge-sm badge-outline badge-info">
+            Protein engineering
+          </div>
+          <div className="badge badge-sm badge-outline badge-info">
+            3D protein modelling
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+function BenjiP() {
+  return (
+    <>
+      <BenjiH1></BenjiH1>
+      <BenjiHeadline></BenjiHeadline>
+      <BenjiTags></BenjiTags>
+    </>
+  );
+}
+
+export default BenjiP;
---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+// assets
+import logo from "../../../public/logo.svg";
+import sun from "../../../public/sun.svg";
+import moon from "../../../public/moon.svg";
+
+const NavBar = () => {
+  // use theme from local storage if available or set light theme
+  const [theme, setTheme] = useState("dark");
+
+  // useEffect(() => {
+  //   let value = localStorage.getItem("theme")
+  //     ? localStorage.getItem("theme")
+  //     : "light";
+  //   setTheme(value);
+  // });
+
+  // set theme state in localstorage on mount & also update localstorage on state change
+  useEffect(() => {
+    let value = localStorage.getItem("theme");
+    //   ? localStorage.getItem("theme")
+    //   : "";
+    // setTheme(value);
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    // add custom data-theme attribute to html tag required to update theme using DaisyUI
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+
+  // update state on toggle
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <div className="sticky top-0 z-20 backdrop-filter backdrop-blur-lg bg-opacity-5">
+      <div className="navbar shadow-lg px-4 sm:px-8">
+        <div className="flex-1">
+          <Image src={logo} alt="OM" className="btn btn-ghost w-24 p-0" />
+          <h1 className="text-xl font-extrabold mx-4">Research</h1>
+        </div>
+        <div className="flex-none">
+          {/* Toggle button here */}
+          <button className="btn btn-square btn-ghost">
+            <label className="swap swap-flip w-24 h-6">
+              <input
+                type="checkbox"
+                onChange={handleToggle}
+                // show toggle image based on localstorage theme
+                checked={theme === "light" ? false : true}
+              />
+              {/* light theme sun image */}
+              <Image src={sun} alt="light" className="w-16 h-6 swap-on" />
+              {/* dark theme moon image */}
+              <Image src={moon} alt="dark" className="w-16 h-6 swap-off" />
+            </label>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
